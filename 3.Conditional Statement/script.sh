64b0470bdf4fcdad0e3ec689dab2cc3736a4103b
M.K.PRAMOD <pramodkavindu44@gmail.com>
2025-01-26 12:51:37 +0530

    if else elseif condtion

---
+++ 3.Conditional Statement/script.sh
@@ -0,0 +1,47 @@
+
+count=10
+
+# if [ $count -eq 10 ]
+# then
+#     echo " condition is true"
+# else 
+#     echo " condition is false"
+# fi 
+
+# if [ $count -ne 10 ]
+# then
+#     echo " condition is true"
+# else 
+#     echo " condition is false"
+# fi
+
+# if (($count > 9 ))
+# then
+#     echo " condition is true"
+# else 
+#     echo " condition is false"
+# fi
+
+# if (($count < 9 ))
+# then
+#     echo " First condition is true"
+# elif  (($count > 9 ))
+# then
+#     echo " Second condition is false"
+# else
+#     echo " condition is false"
+# fi
+
+# if [ "$count" -gt 9 ] && [ "$count" -lt 11 ]
+# then
+#     echo " Condition is true"
+# else
+#     echo " condition is false"
+# fi
+
+if [ "$count" -gt 9 ] || [ "$count" -lt 11 ]
+then
+    echo " Condition is true"
+else
+    echo " condition is false"
+fi
