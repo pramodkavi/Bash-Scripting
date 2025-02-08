2da17b50085744192f49e05f38bea9ab8931e5f5
M.K.PRAMOD <pramodkavindu44@gmail.com>
2025-02-08 15:30:59 +0530

    Basic Operation

---
+++ 9.Numbers&Arithmatics/script.sh
@@ -0,0 +1,15 @@
+
+num1=10
+num2=25
+
+echo $((num1 + num2))
+echo $((num1 - num2))
+echo $((num1 / num2))
+echo $((num1 * num2))
+echo $((num1 % num2))
+echo " "
+echo $(expr $num1 + $num2)
+echo $(expr $num1 - $num2)
+echo $(expr $num1 / $num2)
+echo $(expr $num1 \* $num2)
+echo $(expr $num1 % $num2)
